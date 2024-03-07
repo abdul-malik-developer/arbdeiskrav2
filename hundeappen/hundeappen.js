1b48a0b5756d42adcb3c19cb76c87f3239119b78
Markman555 <markflemminga@gmail.com>
2024-03-07 10:12:16 +0100

    fertch funksjoner

--- hundeappen/hundeappen.js
+++ hundeappen/hundeappen.js
@@ -1,11 +1,18 @@
-const randomUser = 'https://randomuser.me/api/';
-const cardsContainer = document.getElementById('cardsContainer');
-const newCards = document.querySelector('.singel-btn');
-const breedDropdown = document.getElementById('hundeFiltering');
-const filterBtn = document.getElementById('filterBtn');
-const randomGreeting = ['Voff voff', 'Grrr!', 'Mjau??', 'Voff!', 'Voff voff voff', 'WRAFF!!!'];
-const chattBoxImg = document.getElementById('chatBtnImg')
-const chatParentContainer = document.querySelector('.chat-container');
+const randomUser = "https://randomuser.me/api/";
+const cardsContainer = document.getElementById("cardsContainer");
+const newCards = document.querySelector(".singel-btn");
+const breedDropdown = document.getElementById("hundeFiltering");
+const filterBtn = document.getElementById("filterBtn");
+const randomGreeting = [
+  "Voff voff",
+  "Grrr!",
+  "Mjau??",
+  "Voff!",
+  "Voff voff voff",
+  "WRAFF!!!",
+];
+const chattBoxImg = document.getElementById("chatBtnImg");
+const chatParentContainer = document.querySelector(".chat-container");
 
 async function fetchDogImage(url) {
   try {
