563a02e6b9bcd2fe1e51aef579d528dec2efbc88
Markman555 <markflemminga@gmail.com>
2024-03-07 10:03:39 +0100

    Legger inn fetch api funksjoner

--- hundeappen/hundeappen.js
+++ hundeappen/hundeappen.js
@@ -6,3 +6,23 @@ const filterBtn = document.getElementById('filterBtn');
 const randomGreeting = ['Voff voff', 'Grrr!', 'Mjau??', 'Voff!', 'Voff voff voff', 'WRAFF!!!'];
 const chattBoxImg = document.getElementById('chatBtnImg')
 const chatParentContainer = document.querySelector('.chat-container');
+
+async function fetchDogImage(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.message;
+  } catch (error) {
+    console.error("Error fetching dog image:", error);
+  }
+}
+
+async function fetchRandomUser() {
+  try {
+    const response = await fetch(randomUser);
+    const data = await response.json();
+    return data.results[0];
+  } catch (error) {
+    console.error("Error fetching random user:", error);
+  }
+}
